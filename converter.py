387ac9e2a75493f66358d9fb4395b9fd36ca6c8f
nov12 <nov12@qq.com>
2025-02-11 15:59:03 +0800

    增强HTML生成：为HTML添加容器样式，支持替换现有<head>标签，并在标题下方添加时间戳

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 import markdown
 import pandas as pd
@@ -25,28 +26,49 @@ class Coverter:
     def add_html_header(html: str, title: str) -> str:
         """
         为HTML增加网页头部的信息和表格样式以及标题
+        如果存在head内容则替换，包括标题
         """
         head = """
         <head>
             <title>{title}</title>
             <style>
+                .container {{
+                    margin: 0 auto;
+                    width: fit-content;
+                }}
                 table {{
                     border-collapse: collapse;
                     border: 1px solid black;
                     border-spacing: 0;
+                    margin: 0;
                 }}
                 th, td {{
                     border: 1px solid black;
                     padding: 5px;
                 }}
+                h1 {{
+                    text-align: center;
+                }}
+                .timestamp {{
+                    text-align: left;
+                }}
+            </style>
         </head>
         """.format(
             title=title
         )
-        body = f"<body><h1>{title}</h1>{html}</body>"
-        html = f"<html>{head}{body}</html>"
+        body = f'<body><div class="container"><h1>{title}</h1>{html}</div></body>'
 
-        return html
+        # 检查是否已经存在 <head> 标签
+        if "<head" in html:
+            # 使用正则表达式替换 <head> 标签及其内容
+            new_html = re.sub(r"<head.*?</head>", head, html, flags=re.DOTALL)
+            # 替换 <h1> 标签中的标题
+            new_html = re.sub(r"<h1>.*?</h1>", f"<h1>{title}</h1>", new_html)
+        else:
+            new_html = f"<html>{head}{body}</html>"
+
+        return new_html
 
     @staticmethod
     def add_timestamp(html: str) -> str:
@@ -54,11 +76,12 @@ class Coverter:
         为HTML增加时间戳
         """
         now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        time_stamp = f"<div style='text-align: right;'>制表时间: {now}</div>"
-        if "</body>" in html:
-            html = html.replace("</body>", f"{time_stamp}</body>")
+        time_stamp = f'<div class="timestamp">制表时间: {now}</div>'
+        if "</h1>" in html:
+            html = html.replace("</h1>", f"</h1>{time_stamp}")
         else:
             html = f"{html}{time_stamp}"
+        return html
 
     @staticmethod
     def df2md(df: pd.DataFrame) -> str:
