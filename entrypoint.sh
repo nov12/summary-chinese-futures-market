01a179ac0ad5a17391f7ac73fcbf0c1213f28f98
nov12 <nov12@qq.com>
2025-02-19 20:07:07 +0800

    修复错误：entrypoint脚本每次根据PYPATH设置PYTHONPATH，简化环境变量配置逻辑

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-if [ ! -f /tmp/py_env_set ]; then
-  # 如果 PYTHONPATH 没有设置，则将其赋值为 PYPATH，否则将 PYPATH 追加到现有 PYTHONPATH 前面
-  if [ -z "$PYTHONPATH" ]; then
-    export PYTHONPATH="$PYPATH"
-  else
-    export PYTHONPATH="$PYPATH:$PYTHONPATH"
-  fi
-  # 创建标记文件表明已经执行过环境变量设置
-  touch /tmp/py_env_set
+# 每次都根据 PYPATH 设置 PYTHONPATH
+if [ -z "$PYTHONPATH" ]; then
+  export PYTHONPATH="$PYPATH"
+else
+  export PYTHONPATH="$PYPATH:$PYTHONPATH"
 fi
 
 # 执行传入的命令
