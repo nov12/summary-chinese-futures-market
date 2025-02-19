cb1084b7c6c9746eee05b29cc3ec905ed58322a7
nov12 <nov12@qq.com>
2025-02-19 19:36:55 +0800

    更新entrypoint脚本：添加环境变量设置标记，避免重复设置PYTHONPATH

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-# 如果 PYTHONPATH 没有设置，则将其赋值为 PYPATH，否则将 PYPATH 追加到现有 PYTHONPATH 前面
-if [ -z "$PYTHONPATH" ]; then
-  export PYTHONPATH="$PYPATH"
-else
-  export PYTHONPATH="$PYPATH:$PYTHONPATH"
+if [ ! -f /tmp/py_env_set ]; then
+  # 如果 PYTHONPATH 没有设置，则将其赋值为 PYPATH，否则将 PYPATH 追加到现有 PYTHONPATH 前面
+  if [ -z "$PYTHONPATH" ]; then
+    export PYTHONPATH="$PYPATH"
+  else
+    export PYTHONPATH="$PYPATH:$PYTHONPATH"
+  fi
+  # 创建标记文件表明已经执行过环境变量设置
+  touch /tmp/py_env_set
 fi
 
 # 执行传入的命令
